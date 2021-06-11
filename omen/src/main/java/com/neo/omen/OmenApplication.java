024bc7f15be6498fbd333dd4e3263c6397803914
Neokwok <gcc1997@qq.com>
2021-06-12 01:07:04 +0800

    Prompt for improvement

--- omen/src/main/java/com/neo/omen/OmenApplication.java
+++ omen/src/main/java/com/neo/omen/OmenApplication.java
@@ -1,15 +1,20 @@
 package com.neo.omen;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+
+@Slf4j
 @SpringBootApplication
 @MapperScan("com.neo.omen.mapper")
 public class OmenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OmenApplication.class, args);
+        log.info("程序成功启动，请在浏览器中打开 http://localhost:8080/omen 启动批量完成操作");
     }
 
 }
