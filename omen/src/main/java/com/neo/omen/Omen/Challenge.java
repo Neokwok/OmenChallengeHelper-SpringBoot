ca3365efcb87f22a2104d9390c04a61da4e62e5d
Neokwok <gcc1997@qq.com>
2021-07-06 16:07:57 +0800

    Add processing method for "Task one out of two"

--- omen/src/main/java/com/neo/omen/Omen/Challenge.java
+++ omen/src/main/java/com/neo/omen/Omen/Challenge.java
@@ -12,14 +12,11 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
- * @Author jiyec
- * @Date 2021/5/27 12:21
+ * @Author neo
+ * @Date 2021/7/27 15:30
  * @Version 1.0
  **/
 @Slf4j
@@ -47,24 +44,36 @@ public class Challenge {
         Map<String, Object> body = new AllChallengeListBody(applicationId, sessionToken).genBody();
         List<String[]> allList = new ArrayList<>();
         HttpUtilEntity ret = request(body);
-        if(ret == null)return allList;
+        if (ret == null) return allList;
 
         Map listMap = JsonUtil.string2Obj(ret.getBody(), Map.class);
         List<Map<String, Object>> collection = (List<Map<String, Object>>) (((Map) listMap.get("result")).get("collection"));
-
+        //存储临时 campaignId,去重
+        HashSet<String> currentCampaignIdtSet = new HashSet();
         collection.forEach(item -> {
             String challengeStructureId = (String) (item.get("challengeStructureId"));
             List<String> relevantEvents = (List<String>) item.get("relevantEvents");
             Map<String, Object> prize = (Map<String, Object>) (item.get("prize"));
+            //log.info(String.valueOf(prize));
             String category = (String) prize.get("category");
             String campaignId = (String) prize.get("campaignId");
-            if ("sweepstake".equals(category)) {
-                allList.add(new String[]{
-                        challengeStructureId,
-                        campaignId,
-                        relevantEvents.get(0)
-                });
+            //log.info("任务id:"+campaignId);
+            synchronized (campaignId) {
+                if ("sweepstake".equals(category)) {
+                    if (!currentCampaignIdtSet.contains(campaignId)) {
+                        allList.add(new String[]{
+                                challengeStructureId,
+                                campaignId,
+                                relevantEvents.get(0)
+                        });
+                        currentCampaignIdtSet.add(campaignId);
+                        log.info("挑战Id:" + challengeStructureId + ",任务Id:" + campaignId + ",游玩任务:" + relevantEvents.get(0));
+                    }
+
+
+                }
             }
+
         });
 
         return allList;
@@ -73,17 +82,18 @@ public class Challenge {
     // 参加挑战
     public boolean join(String challengeStructureId, String campaignId) {
         Map<String, Object> body = new JoinChallengeBody(applicationId, sessionToken).genBody(campaignId, challengeStructureId);
+        //log.info("任务id"+campaignId);
         HttpUtilEntity ret = request(body);
-        if(ret == null)return false;
+        if (ret == null) return false;
         return true;
     }
 
     // 获取进行中的任务
-    public List<Map<String, Object>> currentList(){
+    public List<Map<String, Object>> currentList() {
         Map<String, Object> body = new CurrentChallengeListBody(applicationId, sessionToken).genBody();
         List<Map<String, Object>> currentList = new ArrayList<>();
         HttpUtilEntity ret = request(body);
-        if(ret == null) return null;
+        if (ret == null) return null;
 
         Map listMap = JsonUtil.string2Obj(ret.getBody(), Map.class);
         List<Map<String, Object>> collection = (List<Map<String, Object>>) (((Map) listMap.get("result")).get("collection"));
@@ -91,10 +101,10 @@ public class Challenge {
         collection.forEach(item -> {
             List<String> relevantEvents = (List<String>) item.get("relevantEvents");
             Map<String, Object> prize = (Map<String, Object>) (item.get("prize"));
-            int progressPercentage = (int)item.get("progressPercentage");
+            int progressPercentage = (int) item.get("progressPercentage");
             String category = (String) prize.get("category");
             if ("sweepstake".equals(category)) {
-                currentList.add(new HashMap<String, Object>(){{
+                currentList.add(new HashMap<String, Object>() {{
                     put("eventName", relevantEvents.get(0));
                     put("progress", progressPercentage);
                 }});
@@ -111,7 +121,7 @@ public class Challenge {
         // 发送请求
         log.info("开始发送请求");
         HttpUtilEntity ret = request(body);
-        if(ret == null)return null;
+        if (ret == null) return null;
         log.info("请求完毕");
 
         // 解析结果
@@ -123,7 +133,7 @@ public class Challenge {
         int percentage = (int) item.get("progressPercentage");
 
         log.info("事件：{} -- 已完成 {}%", relevantEvents, percentage);
-        return new HashMap<String, Object>(){{
+        return new HashMap<String, Object>() {{
             put("progress", percentage);
         }};
     }
@@ -137,13 +147,13 @@ public class Challenge {
                     HEADERS
             );
             int code = ret.getStatusCode();
-            if(code == 400){
+            if (code == 400) {
                 Map result = JsonUtil.string2Obj(ret.getBody(), Map.class);
                 Map<String, Object> error = (Map<String, Object>) result.get("error");
                 log.info("SESSION过期，请更新 - 响应码：{}, {}", ret.getStatusCode(), error.get("message"));
                 System.exit(-1);
                 return null;
-            }else if (code != 200) {
+            } else if (code != 200) {
                 log.info("响应异常-响应码：{}, {}", ret.getStatusCode(), ret);
                 return null;
             }
